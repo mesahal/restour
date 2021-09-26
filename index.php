fed081d0c5dc966086fa8824dc49a6b7807a5c79
sahal3000 <me.sahal3000@gmail.com>
2021-09-26 15:47:10 +0600

    commit 6

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
  
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcgit dn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
  <link rel="preconnect" href="https://fonts.gstatic.com">
 
     <title>Document</title>
-    <!-- Hello testing github from 2nd pc -->
+    <!-- Hello testing team memers pc -->
 </head>
 <body>
     <div class="header">
